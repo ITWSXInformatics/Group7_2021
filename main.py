cb6fa09f83cb2dfc744bc1ea767ea6dd2fdbaee1
catherine-tate <catherine21tate@gmail.com>
2021-04-09 16:46:24 -0400

    map now displays

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
-import geopandas
+import pandas as pd
+
+usa = gpd.read_file('./map/UScounties.shp')
+
+
+usa.plot()
+
+plt.show()
