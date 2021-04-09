4f752aedb477e31b8066438497ba9d8e817f5422
catherine-tate <catherine21tate@gmail.com>
2021-04-09 16:55:09 -0400

    moved data files into data folder

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
-import pandas as pd
 
 usa = gpd.read_file('./map/UScounties.shp')
 
-
 usa.plot()
-
 plt.show()
