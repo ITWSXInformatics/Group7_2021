bc94fae199c293a26747a7aaf6b3c24dcb48e0a1
catherine-tate <catherine21tate@gmail.com>
2021-04-10 15:14:35 -0400

    can now choose between state and county-level view

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
+#import "process.py"
 
-usa = gpd.read_file('./map/UScounties.shp')
+
+mapType = ""
+while (mapType != "state" and mapType != "county"):
+	mapType = input("State or County view? ")
+
+
+if mapType == "county":
+	usa = gpd.read_file('./map/counties/UScounties.shp')
+elif mapType == "state":
+	usa = gpd.read_file('./map/states/States_shapefile.shp')
+
 
 usa.plot()
 plt.show()
