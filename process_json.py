41c9749f597e8f2daf7f349e06fa578cc815d957
lucasfs2020 <44509169+lucasfs2020@users.noreply.github.com>
2021-04-14 17:47:58 -0400

    Update process_json.py
    Added function headers to the code

--- process_json.py
+++ process_json.py
@@ -1,6 +1,9 @@
 import json
 import pandas
 
+'''
+Helper function to create a dictionary, should only be called within this file
+'''
 def create_dict(vac_dict):
 	state_dict = dict()
 	for item in vac_dict["data"]:
@@ -10,6 +13,12 @@ def create_dict(vac_dict):
 			state_dict[item[8]] += float(item[10])
 	return state_dict
 
+'''
+Converts json files to dictionaries
+Takes in nothing, returns dictionary with keys being state name, and the value is the amount of vaccines distributed
+Example:
+full_vac_dict['Alaska'] = some number
+'''
 def convert_json():
 	with open("Data/janssen_distribution.json") as file:
 		janssen_dict = json.loads(file.read())
@@ -31,6 +40,14 @@ def convert_json():
 	for key in janssen_vaccine_dict.keys():
 		full_vac_dict[key] = janssen_vaccine_dict[key]+moderna_vaccine_dict[key]+pfizer_vaccine_dict[key]
 
+	return full_vac_dict
+
+'''
+Converts xlsx file to dictionaries through use of panda DataFrame
+returns two dictionaries:
+1. a condition dictionary showing the underlying conditions per state, condition_dict['Alaska'] = some number
+2. a population dictionary containing the population from 2018 that are 18 and older, population_dict['New York'] = some number
+'''
 def convert_excel():
 	data = pandas.read_excel("Data/underlying conditions.xlsx")
 	condition_data = pandas.DataFrame(data, columns= ['STATE_NAME', 'county_pop2018_18 and older', 'anycondition_number'])
@@ -48,6 +65,11 @@ def convert_excel():
 
 	return condition_dict, population_dict
 
+'''
+Converts csv file to dictionaries through use of panda DataFrame
+returns a dictionary containing the covid vaccines in that state
+condition_dict['Alabama'] = some number
+'''
 def convert_csv():
 	data = pandas.read_csv("Data/covid19_vaccinations_in_the_united_states.csv", sep='delimiter', header=None, engine='python')
 	vaccine_data = pandas.DataFrame(data)
@@ -62,7 +84,10 @@ def convert_csv():
 		count += 1
 
 	return vaccine_dict
-	
+
+'''
+Main function used to test
+'''	
 if __name__ == "__main__":
 	convert_json()
 	convert_excel()
