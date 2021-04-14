0fd84cad2194b3ea59f75fd6b96d1c7b6b911152
lucasfs2020 <44509169+lucasfs2020@users.noreply.github.com>
2021-04-14 13:18:45 -0400

    Update process_json.py
    Finished data conversion for excel and csv

--- process_json.py
+++ process_json.py
@@ -31,5 +31,39 @@ def convert_json():
 	for key in janssen_vaccine_dict.keys():
 		full_vac_dict[key] = janssen_vaccine_dict[key]+moderna_vaccine_dict[key]+pfizer_vaccine_dict[key]
 
+def convert_excel():
+	data = pandas.read_excel("Data/underlying conditions.xlsx")
+	condition_data = pandas.DataFrame(data, columns= ['STATE_NAME', 'county_pop2018_18 and older', 'anycondition_number'])
+	condition_dict = dict()
+	population_dict = dict()
+	for index, row in condition_data.iterrows():
+		if row['STATE_NAME'] not in condition_dict.keys():
+			condition_dict[row['STATE_NAME']] = float(row['anycondition_number'])
+		else:
+			condition_dict[row['STATE_NAME']] += float(row['anycondition_number'])
+		if row['STATE_NAME'] not in population_dict.keys():
+			population_dict[row['STATE_NAME']] = float(row['county_pop2018_18 and older'])
+		else:
+			population_dict[row['STATE_NAME']] += float(row['county_pop2018_18 and older'])
+
+	return condition_dict, population_dict
+
+def convert_csv():
+	data = pandas.read_csv("Data/covid19_vaccinations_in_the_united_states.csv", sep='delimiter', header=None, engine='python')
+	vaccine_data = pandas.DataFrame(data)
+	vaccine_dict = dict()
+	count = 0
+	for index, row in vaccine_data.iterrows():
+		if count >= 3:
+			current_data = row.to_string().split(',')
+			current_state = current_data[0][4:]
+			if current_state not in vaccine_dict.keys():
+				vaccine_dict[current_state] = float(current_data[1])
+		count += 1
+
+	return vaccine_dict
+	
 if __name__ == "__main__":
 	convert_json()
+	convert_excel()
+	convert_csv()
